b4ac4cbc2723461e71b367ac49b217428a661c1a
Gonduls <pierluigi.negro.00@gmail.com>
2021-07-07 17:04:23 +0200

    input try

--- main.c
+++ main.c
@@ -1,25 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define INPUT stdin //"./tests/open_tests/input_1"
+
+#define MAX_NUM_LEN 10
 
 int main(){
-    char c;
-    int i;
-    FILE* input;
-    input = fopen(INPUT, "r");
-    printf("hi\n");
-    i = fscanf(input, "%c", &c);
-    //c = fgetc(input);
-    printf("%c", c);
-
-    //c = getc(stdin);
-    while(i != EOF){
-        printf("%c", c);
-        //c = getc(stdin);
-        i = fscanf(input, "%c", &c);
-        //c = fgetc(input);
+    unsigned D, K;
+    if(scanf("%u %u", &D, &K) != 2){
+        printf("Failed to get input, closing program\n");
+        return 1;
     }
-    unsigned int a = __UINT32_MAX__;
-    printf("%u\n", a);
+
+    unsigned MAX_LINE_LEN = D*(MAX_NUM_LEN + 1);
+    char line[MAX_LINE_LEN];
+    while(fgets(line, MAX_LINE_LEN, stdin) != NULL)
+        printf("%s", line);
     return 0;
 }
