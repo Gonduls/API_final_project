1b30e8327a677203546e272c43babb170032bc93
Gonduls <pierluigi.negro.00@gmail.com>
2021-07-07 23:49:08 +0200

    input not working

--- main.c
+++ main.c
@@ -1,18 +1,95 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define MAX_NUM_LEN 10
+#define HI printf("hi\n");
+#define a_capo printf("\n");
+
+typedef struct {
+    unsigned posizione;
+    unsigned somma;
+} papabile;
+
+typedef struct {
+    papabile* array;
+    unsigned riempimento;
+} migliori;
+
+void print_top(migliori* lista);
+void dijkstra(unsigned *matrix, char* line, int dim, unsigned posizione);
+void input_matrix(unsigned *matrix, char* line, int dim);
+
 
 int main(){
-    unsigned D, K;
-    if(scanf("%u %u", &D, &K) != 2){
+    int D, K;
+    if(scanf("%i %i\n", &D, &K) != 2){
         printf("Failed to get input, closing program\n");
         return 1;
     }
 
-    unsigned MAX_LINE_LEN = D*(MAX_NUM_LEN + 1);
+    unsigned MAX_LINE_LEN = (unsigned) D*(MAX_NUM_LEN + 1);
     char line[MAX_LINE_LEN];
-    while(fgets(line, MAX_LINE_LEN, stdin) != NULL)
-        printf("%s", line);
+    unsigned grafo[D][D - 1];
+    char* aggiungi = "AggiungiGrafo\n", *topk = "TopK\n";
+    unsigned i = 0;
+
+    while(fgets(line, MAX_LINE_LEN, stdin) != NULL){
+        //printf("%s", line);
+        //printf("top: %d, aggiungi: %d\n", strcmp(line, topk), strcmp(line, aggiungi));
+        if(strcmp(line, aggiungi) == 0){
+            //dijkstra(grafo, line, D, i);
+            input_matrix(grafo[0], line, D);
+            i ++;
+            continue;
+        }
+        if(strcmp(line, topk) == 0){
+            continue;
+        }
+        printf("\terrore\n");
+    }    
     return 0;
+}
+
+void input_matrix(unsigned *matrix, char* line, int dim){
+    int i, j, row;
+    unsigned MAX_LINE_LEN = dim*(MAX_NUM_LEN + 1);
+    char *token;
+    for (i= 0; i < dim; i++){
+        if(fgets(line, MAX_LINE_LEN, stdin) != NULL){
+
+            token = strtok(line, ","); // non mi serve il primo numero di ogni riga: è la distanza dal nodo 0, inutile
+            j = 0;
+            row = i*dim;
+            while(token != NULL){
+    HI
+                token = strtok(NULL, ",");
+                matrix[row + j] = atoi(token);
+                printf("%u, ", matrix[row + j]);
+                j++;
+            }
+            a_capo;
+        }
+        else
+            printf("errore 2\n");
+    }
+}
+
+
+void print_top(migliori* lista){
+    unsigned i;
+    if (lista->riempimento == 0){
+        printf("\n");
+        return;
+    }
+    printf("%u", lista->array->posizione);
+
+    for(i = 1; i< lista->riempimento; i++)
+        printf(" %u", lista->array[i].posizione);
+}
+
+void dijkstra(unsigned *matrix, char* line, int dim, unsigned posizione){
+    //unsigned i;
+
 }
